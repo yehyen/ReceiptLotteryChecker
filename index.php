5b3b0ee461f7bec8403f8e578638aeb923d98578
YEH <62413455+yehyen@users.noreply.github.com>
2020-05-29 15:28:17 +0800

    revised index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
-    <div class="container">
+    <form class="container">
         <header>
             <?php
             require("header.html");
@@ -29,7 +29,7 @@
             require("section.html");
             ?>
         </section>
-    </div>
+</form>
 
 </body>
 </html>
